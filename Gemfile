3a4a26b710737971a261741b7087315ee9e81db9
Anton Maminov <anton.linux@gmail.com>
2023-05-21 11:32:58 +0300

    add simplecov

--- Gemfile
+++ Gemfile
@@ -17,4 +17,5 @@ end
 group :test do
   gem 'vcr'
   gem 'webmock'
+  gem 'simplecov'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@
 
 require 'vcr'
 
+require 'simplecov'
+SimpleCov.start
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
